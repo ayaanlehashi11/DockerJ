fd8b703027917072239b8efe2fc705e4bbebd57d
0dayx16 <91608871+ayaanlehashi11@users.noreply.github.com>
2023-12-22 13:42:42 +0300

    Update AES256.java

--- src/main/java/com/ayaanle/AES256.java
+++ src/main/java/com/ayaanle/AES256.java
@@ -3,16 +3,108 @@ package com.ayaanle;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.SecureRandom;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+import java.io.UnsupportedEncodingException;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class AES256 {
+
+  private static final int KEY_LENGTH = 256;
+  private static final int ITERATION_COUNT = 65536;
+
+  public static String encrypt(String strToEncrypt, String secretKey, String salt) {
+
+    try {
+
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] iv = new byte[16];
+        secureRandom.nextBytes(iv);
+        IvParameterSpec ivspec = new IvParameterSpec(iv);
+
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        KeySpec spec = new PBEKeySpec(secretKey.toCharArray(), salt.getBytes(), ITERATION_COUNT, KEY_LENGTH);
+        SecretKey tmp = factory.generateSecret(spec);
+        SecretKeySpec secretKeySpec = new SecretKeySpec(tmp.getEncoded(), "AES");
+
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivspec);
+
+        byte[] cipherText = cipher.doFinal(strToEncrypt.getBytes("UTF-8"));
+        byte[] encryptedData = new byte[iv.length + cipherText.length];
+        System.arraycopy(iv, 0, encryptedData, 0, iv.length);
+        System.arraycopy(cipherText, 0, encryptedData, iv.length, cipherText.length);
+
+        return Base64.getEncoder().encodeToString(encryptedData);
+    } catch (Exception e) {
+        // Handle the exception properly
+        e.printStackTrace();
+        return null;
     }
+  }
+
+public static String decrypt(String strToDecrypt, String secretKey, String salt) {
+
+    try {
+
+        byte[] encryptedData = Base64.getDecoder().decode(strToDecrypt);
+        byte[] iv = new byte[16];
+        System.arraycopy(encryptedData, 0, iv, 0, iv.length);
+        IvParameterSpec ivspec = new IvParameterSpec(iv);
+
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        KeySpec spec = new PBEKeySpec(secretKey.toCharArray(), salt.getBytes(), ITERATION_COUNT, KEY_LENGTH);
+        SecretKey tmp = factory.generateSecret(spec);
+        SecretKeySpec secretKeySpec = new SecretKeySpec(tmp.getEncoded(), "AES");
+
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivspec);
+
+        byte[] cipherText = new byte[encryptedData.length - 16];
+        System.arraycopy(encryptedData, 16, cipherText, 0, cipherText.length);
+
+        byte[] decryptedText = cipher.doFinal(cipherText);
+        return new String(decryptedText, "UTF-8");
+    } catch (Exception e) {
+        // Handle the exception properly
+        e.printStackTrace();
+        return null;
+    }
+  }
+
+    public static void main(String[] args) {
+
+    // Define your secret key and salt (keep these secure and don't hardcode in production)
+    String secretKey = "AyaanleMaxamed";
+    String salt = "Hashi";
+
+    // String to be encrypted
+    String message = "Tiro tijaabo : Kow laba saddex .";
+
+    // Encrypt the string
+    String encryptedString = AES256.encrypt(originalString, secretKey, salt);
+    if (encryptedString != null) {
+        System.out.println("Encrypted: " + encryptedString);
+    } else {
+        System.err.println("Encryption failed.");
+        return;
+    }
+
+    // Decrypt the string
+    String decryptedString = AES256.decrypt(encryptedString, secretKey, salt);
+    if (decryptedString != null) {
+        System.out.println("Decrypted: " + decryptedString);
+    } else {
+        System.err.println("Decryption failed.");
+    }
+  }
+
+
+
 }
